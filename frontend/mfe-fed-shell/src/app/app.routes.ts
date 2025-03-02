c1c8cdc1bcc911fe5b71cafd865acc02c0cfbd9d
Mateus Macedo <mateusouza2014@live.com>
2025-03-02 20:09:54 -0300

    feat: Adicionando projeto root shell

---
+++ frontend/mfe-fed-shell/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+  {
+    path: 'dashboard-admin',
+    loadComponent: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Component', // Expondo o componente standalone
+      }).then((m) => m.AppComponent), // Nome do componente standalone
+  },
+  {
+    path: 'dashboard-user',
+    loadComponent: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4202/remoteEntry.js',
+        exposedModule: './Component', // Expondo o componente standalone
+      }).then((m) => m.AppComponent), // Nome do componente standalone
+  },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
