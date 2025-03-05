c216e076fd814b0fc58f023e7fdd96528182866b
Mateus Macedo <mateusouza2014@live.com>
2025-03-05 09:24:33 -0300

    feat: Adicionando component de login e classes de serviços para authenticação do usuario

--- frontend/mfe-fed-shell/src/app/app.routes.ts
+++ frontend/mfe-fed-shell/src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
   /*{
@@ -24,5 +27,17 @@ export const routes: Routes = [
   },
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },*/
-  { path: 'login', component: LoginComponent }
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignUpComponent
+  },
+  {
+    path: "user",
+    component: UserComponent,
+    canActivate: [AuthGuard]
+  }
 ];
